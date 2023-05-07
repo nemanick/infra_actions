5c3f8aab4c2f34c71a04490db95e433ba3d302bb
Иван Неманов <kadetivan2019@yandex.ru>
2023-05-07 16:25:58 +0700

    deploy make v2

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получился деплой!')
+    return HttpResponse('У меня получится деплой! ;-((')
 
 
 def second_page(request):
